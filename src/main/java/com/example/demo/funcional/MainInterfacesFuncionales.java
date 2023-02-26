732fcdf7fddcc3c68f935b0501ffc8193e5bdf81
BlueFeet <b1feet.dev@uce.edu.ec>
2023-02-25 21:08:09 -0500

    Java Interfaz Funcional

--- src/main/java/com/example/demo/funcional/MainInterfacesFuncionales.java
+++ src/main/java/com/example/demo/funcional/MainInterfacesFuncionales.java
@@ -1,15 +1,11 @@
 package com.example.demo.funcional;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.example.demo.funcional.consumer.IPersonaConsumer;
-import com.example.demo.funcional.consumer.PersonaConsummerImpl;
-import com.example.demo.funcional.function.IPersonaFunction;
-import com.example.demo.funcional.predicate.IPersonaPredicate;
-import com.example.demo.funcional.suplier.IPersonaSuplier;
-import com.example.demo.funcional.suplier.PersonaSuplier2Impl;
-import com.example.demo.funcional.suplier.PersonaSuplierImpl;
 
 public class MainInterfacesFuncionales {
 
@@ -17,79 +13,27 @@ public class MainInterfacesFuncionales {
 
 	public static void main(String[] args) {
 		// 1. SUPLIER
-		LOG.error("SUPLIER");
-		// INTERFAZ
-		IPersonaSuplier<String> persona = new PersonaSuplierImpl();
-		LOG.error("Suplier Clase: " + persona.getNombre());
-
-		IPersonaSuplier<Persona> persona5 = new PersonaSuplier2Impl();
-		LOG.info("Suplier Clase: " + persona5.getNombre());
-
-		// LAMBDAS
-		IPersonaSuplier<String> persona2 = () -> "Serghy";
-		LOG.info("Suplier Lambda: " + persona2.getNombre());
-
-		IPersonaSuplier<Integer> persona3 = () -> Integer.valueOf(5);
-		LOG.info("Suplier Lamda: " + persona3.getNombre());
-
-		IPersonaSuplier<Persona> persona4 = () -> {
-			Persona per = new Persona();
-			per.setApellido("Castillo");
-			per.setNombre("Serghy");
-			return per;
-		};
-		LOG.info("Suplier lambda: " + persona4.getNombre());
-
+		LOG.error("JAVA SUPLIER");
+		Stream<String> lista = Stream.generate(() -> "Serghy").limit(10);
+		lista.forEach(a -> LOG.info(a));
 		// 2. CONSUMER
-		LOG.error("CONSUMMER");
-		// INTERFAZ
-		IPersonaConsumer<String, Integer> consumer = new PersonaConsummerImpl();
-		consumer.accept("Procesa este dato INTERFAZ", 3);
-		// LAMBDA
-		IPersonaConsumer<String, Integer> consumer2 = (a, b) -> LOG.info("Mensaje: " + a + b);
-		consumer2.accept("Procesa este dato LAMBDA", 3);
-
+		LOG.error("JAVA CONSUMMER");
+		List<Integer> listaNumeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		listaNumeros.forEach(a -> LOG.info("Valor: " + a));
 		// 3. PREDICATE
-		LOG.error("PREDICATE");
-		// LAMBDA
-		IPersonaPredicate<String> predicate = a -> a.contains("r");
-		LOG.info("Predicate: " + predicate.evaluate("Serghy"));
-
-		IPersonaPredicate<Integer> predicate2 = a -> {
-			if (a.intValue() >= 10) {
-				return true;
-			} else
-				return false;
-		};
-		LOG.info("Predicate: " + predicate2.evaluate(Integer.valueOf(8)));
-
+		LOG.error("JAVA PREDICATE");
+		Stream<Integer> filtrada = listaNumeros.stream().filter(a -> a >= 3);
+		filtrada.forEach(a -> LOG.info("Valor Filtrado: " + a));
 		// 4. FUNCTION
-		LOG.error("FUNCTION");
-		// LAMBDA
-		IPersonaFunction<String, Integer> function = a -> {
-			return "Edison";
-		};
-		LOG.info("Function: " + function.aplicar(2));
-
-		IPersonaFunction<Ciudadano, Persona> function2 = b -> {
-			Ciudadano ciu = new Ciudadano();
-			ciu.setNombreCompleto(b.getNombre() + " " + b.getApellido());
-			ciu.setCiudad("Quito");
-			return ciu;
-		};
-		Persona persona6 = new Persona();
-		persona6.setNombre("Serghy");
-		persona6.setApellido("Castillo");
-		Ciudadano ciudadano = function2.aplicar(persona6);
-		LOG.info("Function: " + ciudadano);
+		LOG.error("JAVA FUNCTION");
+		Stream<String> listaMappeada = listaNumeros.stream().map(a -> {
+			Integer valor = a + 1;
+			String cadena = "num: " + valor.toString();
+			return cadena;
+		});
+		listaMappeada.forEach(a -> LOG.info(a));
 		// 5. UNARY OPERATION
-		LOG.error("UNARY OPERATOR");
-		// LAMBDA
-		IPersonaUnaryOperator<String> unaryOperator = a -> {
-			return a.concat("-Sufijo");
-		};
-		LOG.info("Unary Operator: " + unaryOperator.aplicar("Daniel"));
-
+		LOG.error("JAVA UNARY OPERATOR");
 	}
 
 }
